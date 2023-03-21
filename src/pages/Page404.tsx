98da4a232eb2b9f4de7fa0409ba0da9b4ac35459
Chris <cschwartz110@gmail.com>
2023-03-21 00:01:58 -0500

    update package.json and 404 styling

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -4,7 +4,7 @@ import styles from './Page404.module.scss';
 export const Page404 = () => {
   return (
     <Page title={"Chris Schwartz - 404"}>
-      <div className={styles.container}>
+      <div className={styles.container404}>
         <h1>404</h1>
 
         <p><strong>Page not found :(</strong></p>
